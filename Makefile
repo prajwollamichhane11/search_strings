7b8cf1abb12ff5448f4763f320a90a73257a2305
Prajwol Lamichhane <prajwollamichhane11@gmail.com>
2018-04-13 06:18:29 +0545

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,5 @@
+#this will compile all the files
+all:
+	gcc Search_Strings.c -o string
+
+compile:
---
+++ Search_Strings.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+static char buf[1000];
+static int count = 0;
+static char *dir_filenames[100];
+
+int directory_files(){
+
+    int length[20];     //file name length for all files
+    int i = 0;
+    int j = 0;
+    int counter  = 0;   //number of files in dir
+
+    DIR *d;             //pointer to wd
+    struct dirent *dir;
+
+    d = opendir(".");
+
+    //this is to store values in the argument vector
+    //argv vector takes all filenames
+    if (d) {
+        while ((dir = readdir(d)) != NULL){
+            if(dir->d_name[0] != '.'){
+                 dir_filenames[i] = dir->d_name;
+                 i++;
+            }
+            counter=i;
+
+        }
+        closedir(d);
+    }
+    j = 0;
+    for(i = 0 ; i<counter ; i++){
+        length[i] = strlen(dir_filenames[i]);
+
+        if(dir_filenames[i][length[i]-1] == 't' && dir_filenames[i][length[i]-2] == 'x' && dir_filenames[i][length[i]-3] == 't'){
+	  //set argv to files with txt only
+	  dir_filenames[j] = dir_filenames[i];
+	  printf("%s\n",dir_filenames[j]);
+	  j++;
+	}
+    }
+
+    counter = j;
+
+    return counter;
+    //checked and is true
+}
+
+
+int compare_word(char *test_string, char *file_path,int init_count){
+
+    int word_start = 0;
+    int j = 0;
+    count = 0;
+
+    while(buf[j]){
+
+        if(j == 0 || buf[j-1] == ' ' || buf[j-1] == '\n'){
+            word_start = 1;
+        }
+        else word_start = 0;
+
+        if(word_start){
+            int m = j;
+            int k = 0;
+            while (test_string[k]){
+                if(test_string[k] == buf[m]){
+                    k++;
+                    m++;
+                }
+                else break;
+            }
+
+            if (k == strlen(test_string)){
+                count++;
+            }
+        }
+        j++;
+    }
+    printf("%s has repeated %d no. of times in %s\n",test_string,count,file_path);
+
+    return count;
+}
+
+int read_file(char *file_path)
+{
+    FILE *ptr_file;
+
+    ptr_file =fopen(file_path,"r");
+
+    if (!ptr_file){
+        return -1;
+    }
+
+    char *A;
+    A = &buf[0];
+
+    do {
+
+        *A = fgetc(ptr_file);
+        if (feof(ptr_file)) {
+            break ;
+        }
+
+        ++A;
+    } while(1);
+
+    fclose(ptr_file);
+    return 0;
+}
+
+
+int main(int argc,char **argv){
+
+    int m = 0;
+    int total = 0;
+    int i = 0;
+    char str[25];
+    int count = 0;
+
+    printf("The Text files in the directory are.:\n");
+    m = directory_files();
+    printf("Enter the string you want to search.\n");
+    scanf("%s",str);
+
+    for(i = 0 ; i < m ; i++){
+      read_file(dir_filenames[i]);
+      count =  compare_word(str,dir_filenames[i],count);
+      total = count  + total;
+    }
+
+     printf("%s has repeated %d no. of times in total.\n",str,total);
+
+    return 0;
+}
